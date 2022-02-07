2f2cca41882d80e80ec8365270b4ca71a9d0f90f
Mohamed Belhaimeur <90259446+MohamedBelhaimeur@users.noreply.github.com>
2022-02-07 11:06:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,6 +33,25 @@ const App = () => {
       dispatch(getPostTrend(uid));
     } // eslint-disable-next-line
   }, [uid]);
+  
+  useEffect(() => {
+    var _paq = (window._paq = window._paq || []);
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
+    (function () {
+      var u = "//f-server.viewdns.net/matomo/";
+      _paq.push(["setTrackerUrl", u + "matomo.php"]);
+      _paq.push(["setSiteId", "1"]);
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.async = true;
+      g.src = u + "matomo.js";
+      s.parentNode.insertBefore(g, s);
+    })();
+    // eslint-disable-next-lin
+  }, []);
 
   return (
     <UidContext.Provider value={uid}>
